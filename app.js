4edbb23c7b9fc004960bcfa85aff891e29cb4ce4
Kamila Nitta <kamila.nitta@gmail.com>
2021-07-29 00:18:48 +0200

    update

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const PORT = 4000;
 const cors = require("cors");
 require("./config/db.config")();
 
@@ -7,10 +8,18 @@ const app = express();
 
 app.use(express.json());
 // Não esquecer de criar variável de ambiente com o endereço do seu app React (local ou deployado no Netlify)
+
+//Test do servidor - kamila
+app.get('/homepage',(req, res)=>{
+  return res.send("Funciona 123")
+})
+
 app.use(cors({ origin: process.env.REACT_APP_URL }));
 
 const userRouter = require("./routes/user.routes");
 app.use("/", userRouter);
+
+
 
 app.listen(Number(process.env.PORT), () =>
   console.log(`Server up and running at port ${process.env.PORT}`)
